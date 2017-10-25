fe69b557716a143b3c9208c5c198649bb2700f5e
s203 <s203@AI241LAB0203M24.local>
2017-10-25 11:48:24 -0700

    updates

---
+++ wall-painting.py
@@ -0,0 +1,9 @@
+print("Do you want to paint a wall?")
+    if Yes:
+        print("How wide are the walls?")
+        wall_convert = int(input())
+        wall_wide = wall_convert
+    print("How tall are the walls?")input()
+        wall_convert = int(input())
+        wall_tall = wall_convert
+        pass
