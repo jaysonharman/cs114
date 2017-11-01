e941ddfb04eeb18778e96ba01e613f024177419b
s203 <s203@AI241LAB0203M24.local>
2017-11-01 12:02:37 -0700

    updates

---
+++ distance.py
@@ -0,0 +1,12 @@
+print("Type in lenth of distance")
+input(distance_num)
+
+print("Enter type of units: mi, ki, ft, or in")
+input(distance_unit)
+
+if distance_unit == mi
+    output = distance_num + distance_unit
+    print (output)
+
+print("Enter type of you want it converted to: mi, ki, ft, or in")
+convert_unit = input()
