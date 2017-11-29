61710b3d35c9a4fa9fed3fa8b403ec9328d74944
Student <student@2024-01-203-09.aipdcls.edu>
2017-11-29 11:50:42 -0800

    updates

---
+++ randomenemy.py
@@ -0,0 +1,27 @@
+import random
+
+print("PRESS ENTER TO BATTLE")
+input()
+
+spam = ['Guard', 'Monster', 'WareWolf', 'Wizard']
+
+
+def battle(spam):
+    randomenemy = random.randint(0,3)
+    if randomenemy == 0:
+        return print ("You have been challenged by " + spam[0] + ", prepare to die!")
+    elif randomenemy == 1:
+        return print ("A " + spam[2] + " has came out of nowhere and attacked you!")
+    elif randomenemy == 2:
+        return print ("A drunken old " + spam[3] + " has attacked you!")
+    elif randomenemy == 3:
+        return print ("You have awoken the " + spam[1] + " prepare to fight til the death!")
+
+    # r = random.randint(1,5)
+    # battle_enemy = battle(r)
+
+
+def main():
+    battle(spam)
+
+main()
